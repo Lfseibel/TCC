a1ee22a8e782d5243fb4a6c93f32be4a123daa0b
Luiz <lfseibel@live.com>
2023-03-25 17:12:24 -0300

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\{Block, Calendar, Reservation, ReservationDate, Room, Schedule, Unity, User};
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,67 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Block::create([
+            'code' => 'C'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Calendar::create([
+            'year' => 2023,
+            'period' => 1,
+            'limitDate' => '2023/07/26'
+        ]);
+
+        Room::create([
+            'code' => 'B1004',
+            'capacity' => 50,
+            'reduced_capacity' => 25,
+            'block_code' => 'C'
+        ]);
+
+        Unity::create([
+            'code' => 'MI',
+            'name' => 'Math institute'
+        ]);
+
+        User::create([
+            'email' => 'lfseibel@live.com',
+            'password' => bcrypt('1234'),
+            'type' => 'admin',
+            'unity_code' => 'MI'
+        ]);
+
+        Schedule::create([
+            'code' => 'N1',
+            'startTime' => '07:00',
+            'endTime' => '07:55'
+        ]);
+
+        Reservation::create([
+            'acronym' => 'DB',
+            'class' => '01',
+            'description' => 'Course of Databases',
+            'observation' => 'Course',
+            'responsible' => 'Olmes',
+            'startTime' => '07:00',
+            'endTime' => '07:55',
+            'room_code' => 'B1004',
+            'user_email' => 'lfseibel@live.com'
+        ]);
+
+        DB::table('reservationDate')->insert([
+            [
+                'date' => '2023-05-01',
+                'reservation_code' => 1,
+            ],
+            [
+                'date' => '2023-05-02',
+                'reservation_code' => 1,
+            ],
+            [
+                'date' => '2023-05-03',
+                'reservation_code' => 1,
+            ]
+        ]);
+
     }
 }
