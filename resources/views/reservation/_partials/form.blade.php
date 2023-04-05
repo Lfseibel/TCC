8f32282cbc14e77d624572346e09917458d91264
Luiz <lfseibel@live.com>
2023-04-05 18:17:41 -0300

    view

--- resources/views/reservation/_partials/form.blade.php
+++ resources/views/reservation/_partials/form.blade.php
@@ -15,24 +15,14 @@
   <option value="Quinzenal">Quinzenal</option>
   <option value="{{ old('times') ?? NULL }}" selected>{{ old('times') ??'Escolha a quantidade de vezes'}} </option>
 </select>
-<select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-unifei-500 focus:border-unifei-500 block w-full p-2.5 my-2" name="weekday">
-  <option value="Segunda-Feira">Segunda-Feira</option>
-  <option value="Terça-Feira">Terça-Feira</option>
-  <option value="Quarta-Feira">Quarta-Feira</option>
-  <option value="Quinta-Feira">Quinta-Feira</option>
-  <option value="Sexta-Feira">Sexta-Feira</option>
-  <option value="Sabado">Sabado</option>
-  <option value="Domingo">Domingo</option>
-  <option value="{{ old('weekday') ?? NULL }}" selected>{{ old('weekday') ??'Escolha o dia da semana (0-6 // Domingo - Sabado)'}} </option>
-</select>
   <input type="text" name="acronym" placeholder="Sigla:" value="{{ $reservation->acronym ?? old('acronym') }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2">
   <input type="text" name="class" placeholder="Turma:" value="{{ $reservation->class ?? old('class') }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2">
   <textarea rows="1" name="description" class="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2" placeholder="Descrição">{{ $reservation->description ?? old('description') }}</textarea>
   <textarea rows="1" name="observation" class="block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2 mt-4" placeholder="Observação">{{ $reservation->observation ?? old('observation') }}</textarea>
   <input type="text" name="responsible" placeholder="Responsável:" value="{{ $reservation->responsible ?? old('responsible') }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2">
   
-  <input type="date" name="startDate" placeholder="Data começo:" value="{{ $reservation->startTime ?? old('startTime') ?? $startTime}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2">
-  <input type="date" name="endDate" placeholder="Data fim:" value="{{ $reservation->endTime ?? old('endTime') ?? $endTime }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2">
+  <input type="date" name="startDate" placeholder="Data começo:" value="{{ $reservation->startDate ?? old('startDate') }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2">
+  <input type="date" name="endDate" placeholder="Data fim:" value="{{ $reservation->endDate ?? old('endDate') }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2">
   <input type="time" name="startTime" placeholder="Horario começo:" value="{{ $reservation->startTime ?? old('startTime') ?? $startTime}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2">
   <input type="time" name="endTime" placeholder="Horario fim:" value="{{ $reservation->endTime ?? old('endTime') ?? $endTime }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-2">
   
